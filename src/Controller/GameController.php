ec00a8cd2fe9c852084441be2319db3f1a120e6e
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-04-20 21:29:32 +0300

    Prepare routes in GameController

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -12,6 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class GameController extends AbstractController
 {
@@ -25,6 +26,7 @@ final class GameController extends AbstractController
     }
 
     #[Route('/game', name: 'app_game_create', methods: ['POST'])]
+    #[IsGranted('ROLE_USER')]
     public function createGame(GameRepository $gameRepository): Response
     {
         $newGame = new Game();
@@ -33,21 +35,66 @@ final class GameController extends AbstractController
         $newGame->setCreatedAt(new \DateTimeImmutable());
         $gameRepository->save($newGame, true);
 
-        return new Response();
+        return $this->redirectToRoute('app_game_lobby', [
+            'id' => $newGame->getId(),
+        ]);
     }
 
-    #[Route('/game/{id}', name: 'app_game_join', methods: ['GET'])]
-    public function joinGame(Game $game): Response
+    #[Route('/game/{id}/lobby', name: 'app_game_lobby', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
+    public function lobby(Game $game): Response
     {
+        return $this->render('/game/lobby.html.twig', [
+            'game' => $game,
+        ]);
+    }
 
-        return new Response();
+    #[Route('/game/{id}/join', name: 'app_game_join', methods: ['POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function joinGame(
+        #[CurrentUser] User $user,
+        Game $game,
+        GameRepository $gameRepository
+    ): Response
+    {
+        $game->setPlayer2($user);
+        $game->setStatus(GameStatus::ACTIVE);
+        $gameRepository->save($game, true);
+
+        return $this->redirectToRoute('app_game_lobby', [
+            'id' => $game->getId(),
+        ]);
+    }
+
+    #[Route('/game/{id}/ship-placement', name: 'app_game_ship_placement', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
+    public function shipPlacement(
+        #[CurrentUser] User $user,
+        Game $game,
+    ): Response
+    {
+        return $this->render('/game/ship_placement.html.twig', [
+            'game' => $game,
+        ]);
+    }
+
+    #[Route('/game/{id}/ship-placement', name: 'app_game_ship_placement_save', methods: ['POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function shipPlacementSave(
+        #[CurrentUser] User $user,
+        Game $game,
+    ): Response
+    {
+        #TODO save board coordinates
+        return $this->redirectToRoute('app_game_ship_placement');
     }
 
     #[Route('/game/{id}/play', name: 'game_play')]
     public function play(Game $game): Response
     {
-
-        return new Response();
+        return $this->render('/game/play.html.twig', [
+            'game' => $game,
+        ]);
     }
 
     public function markPlayerAsReady(
