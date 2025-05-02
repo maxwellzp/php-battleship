dbd972604e950142e56e727a1b0bf347063e8681
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-05-02 15:51:42 +0300

    Create a new service - BoardCreator

---
+++ src/Service/BoardCreator.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\Board;
+use App\Entity\Game;
+use App\Factory\BoardFactory;
+use App\Repository\BoardRepository;
+
+class BoardCreator
+{
+    public function __construct(
+        private BoardRepository $boardRepository,
+        private BoardFactory    $boardFactory,
+    )
+    {
+    }
+
+    /**
+     * @param Game $game
+     * @return Board[] array
+     * @throws \Exception
+     */
+    public function createBoards(Game $game): array
+    {
+        if ($game->getPlayer1() === null) {
+            throw new \Exception('Game requires both player1 and player2. First player is missing.');
+        }
+
+        if ($game->getPlayer2() === null) {
+            throw new \Exception('Game requires both player1 and player2. Second player is missing.');
+        }
+
+        $board1 = $this->boardFactory->create($game, $game->getPlayer1());
+        $this->boardRepository->save($board1, true);
+
+        $board2 = $this->boardFactory->create($game, $game->getPlayer2());
+        $this->boardRepository->save($board2, true);
+
+        return [$board1, $board2];
+    }
+}
