ec3753549fec525c36fb28d5ffde78d9e6e11203
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-05-05 22:01:14 +0300

    Make updateShipSunkStatus() send data to Mercure

--- src/Service/ShipStatusService.php
+++ src/Service/ShipStatusService.php
@@ -5,22 +5,26 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Entity\Ship;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 
 class ShipStatusService
 {
     public function __construct(
-        private readonly EntityManagerInterface $entityManager
+        private readonly EntityManagerInterface $entityManager,
+        private readonly MercureService $mercureService,
     ) {
     }
 
-    public function updateShipSunkStatus(Ship $ship): void
+    public function updateShipSunkStatus(Ship $ship, User $player): void
     {
         $shots = $ship->getBoard()->getShots();
 
         if ($ship->isSunkByShots($shots) && !$ship->isSunk()) {
             $ship->setIsSunk(true);
             $this->entityManager->flush();
+
+            $this->mercureService->publishShipIsSunk($ship, $player->getId()->toString());
         }
     }
 }
